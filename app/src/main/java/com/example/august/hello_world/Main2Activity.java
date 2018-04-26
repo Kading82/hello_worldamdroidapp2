b9ec3782537ff17b824ce38739053c03f12fcc55
kading82 <kading82@gmail.com>
2018-04-26 12:56:35 -0700

    finished project 3 no testing code yet

--- app/src/main/java/com/example/august/hello_world/Main2Activity.java
+++ app/src/main/java/com/example/august/hello_world/Main2Activity.java
@@ -1,44 +1,45 @@
 package com.example.august.hello_world;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // Get the Intent that started this activity and extract the string
+        Bundle extras = getIntent().getExtras();
+        String message = extras.getString(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView3);
+        textView.setText("Thanks for Signing Up " + message +"!");
+
+        String message2 = extras.getString(MainActivity.EXTRA_MESSAGE2);
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setText("Name: " + message2);
+
+        String message3 = extras.getString(MainActivity.EXTRA_MESSAGE3);
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText("Email: " + message3);
+
+        String message4 = extras.getString(MainActivity.EXTRA_MESSAGE4);
+        TextView textView4 = findViewById(R.id.textView4);
+        textView4.setText("Birthday: " + message4);
+
+        String message5 = extras.getString(MainActivity.EXTRA_MESSAGE5);
+        TextView textView5 = findViewById(R.id.textView5);
+        textView5.setText("Age: " + message5);
+
+
     }
 
+    public void button2(View b){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/august/hello_world/MainActivity.java
+++ app/src/main/java/com/example/august/hello_world/MainActivity.java
@@ -1,13 +1,134 @@
 package com.example.august.hello_world;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.example.august.hello_world.R.string.alert;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static String EXTRA_MESSAGE = "pass1";
+    public static String EXTRA_MESSAGE2 = "pass2";
+    public static String EXTRA_MESSAGE3 = "pass3";
+    public static String EXTRA_MESSAGE4 = "pass4";
+    public static String EXTRA_MESSAGE5 = "pass5";
+    DatePickerDialog datePickerDialog;
+    private EditText Name;
+    private EditText Email;
+    private EditText Username;
+    private EditText DOB;
+    private EditText Age;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DOB = (EditText) findViewById(R.id.editText5);
+        // perform click event on edit text
+        DOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(MainActivity.this,android.R.style.Theme_Holo_Dialog,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int day) {
+                                // set day of month , month and year value in the edit text
+                                DOB.setText(month + "/" + day + "/" + year);
+                                Calendar userAge = new GregorianCalendar(year,month,day);
+                                Calendar minAdultAge = new GregorianCalendar();
+                                minAdultAge.add(Calendar.YEAR, -18);
+                                if (minAdultAge.before(userAge)) {
+                                    Snackbar.make(findViewById(R.id.editText5), "You must be at least 18 years or older, the application will now close",
+                                            Snackbar.LENGTH_SHORT)
+                                            .show();
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            finish();
+                                        }
+                                    }, 5000); // Millisecond 1000 = 1 sec
+
+
+                                }
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    //Button call and retrieve username and pass username to second actvity
+    public void onButtonClick(View v) {
+
+        Name = (EditText) findViewById(R.id.editText);
+        Email = (EditText) findViewById(R.id.editText2);
+        Username = (EditText) findViewById(R.id.editText3);
+        Age = (EditText) findViewById(R.id.editText4);
+        DOB = (EditText) findViewById(R.id.editText5);
+
+        boolean checkFormat;
+
+
+        if (Name.length() == 0 || Email.length() == 0 || Username.length() == 0
+                || Age.length() == 0  || DOB.length() == 0) {
+            Snackbar.make(findViewById(R.id.editText), R.string.alert,
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        if (Age.length() > 2){
+            Snackbar.make(findViewById(R.id.editText), "No one over 99 should be dating",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+
+        Intent intent = new Intent(this, Main2Activity.class);
+        Bundle extras = new Bundle();
+        String message = Username.getText().toString();
+        String message2 = Name.getText().toString();
+        String message3 = Email.getText().toString();
+        String message4 = DOB.getText().toString();
+        String message5 = Age.getText().toString();
+        extras.putString(EXTRA_MESSAGE, message);
+        extras.putString(EXTRA_MESSAGE2, message2);
+        extras.putString(EXTRA_MESSAGE3, message3);
+        extras.putString(EXTRA_MESSAGE4, message4);
+        extras.putString(EXTRA_MESSAGE5, message5);
+        intent.putExtras(extras);
+        startActivity(intent);
     }
 }
+
+
+
+
+
+
+
+
