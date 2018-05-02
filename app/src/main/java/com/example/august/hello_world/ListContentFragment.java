fff7eae63119416022b3df754428d47bcf3b2264
kading82 <kading82@gmail.com>
2018-05-02 11:13:08 -0700

    Fixed layout issue

--- app/src/main/java/com/example/august/hello_world/ListContentFragment.java
+++ app/src/main/java/com/example/august/hello_world/ListContentFragment.java
@@ -1,7 +1,20 @@
 package com.example.august.hello_world;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ListContentFragment extends Fragment {
+
+    public ListContentFragment(){
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return null;
+    }
 }
