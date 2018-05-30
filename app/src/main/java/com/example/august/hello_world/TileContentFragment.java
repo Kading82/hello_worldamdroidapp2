3ec393fde9baa12ed4dd6148db08d6f3927f7cb9
kading82 <kading82@gmail.com>
2018-05-30 11:25:50 -0700

    no errors but location text does not display

--- app/src/main/java/com/example/august/hello_world/TileContentFragment.java
+++ app/src/main/java/com/example/august/hello_world/TileContentFragment.java
@@ -3,11 +3,18 @@ package com.example.august.hello_world;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -39,9 +46,9 @@ import java.util.Map;
 
 public class TileContentFragment extends Fragment {
 
-
-    private Firebaseconnect connect = new Firebaseconnect();
-    private ArrayList<GetData> matcheslist = new ArrayList<>();
+    LocationManager locationManager;
+    double longitudeNetwork, latitudeNetwork;
+    TextView textviewlocation;
 
     public TileContentFragment() {
 
@@ -52,6 +59,9 @@ public class TileContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
+        locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Firebaseconnect connect = new Firebaseconnect();
+        ArrayList<GetData> matcheslist = new ArrayList<>();
 
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(
                 R.layout.recycler_view, container, false);
@@ -67,8 +77,81 @@ public class TileContentFragment extends Fragment {
         return recyclerView;
     }
 
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+        dialog.setTitle(R.string.enable_location)
+                .setMessage(getString(R.string.location_message))
+                .setPositiveButton(R.string.location_settings, (paramDialogInterface, paramInt) -> {
+                    Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(myIntent);
+                })
+                .setNegativeButton(R.string.location_cancel, (paramDialogInterface, paramInt) -> {});
+        dialog.show();
+    }
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled()) {
+            showAlert();
+        }
+        return isLocationEnabled();
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+
+    public void toggleNetworkUpdates(View view) {
+        if(!checkLocation()) {
+            return;
+        }
+        Button button = (Button) view;
+        if(button.getText().equals(getResources().getString(R.string.pause))) {
+            locationManager.removeUpdates(locationListenerNetwork);
+            button.setText(R.string.resume);
+        }
+        else {
+            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 10, locationListenerNetwork);
+                Toast.makeText(getActivity(), R.string.network_provider_started_running, Toast.LENGTH_LONG).show();
+                button.setText(R.string.pause);
+            }
+        }
+    }
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+
+            getActivity().runOnUiThread(() -> {
+                textviewlocation.setText(String.format("%s", longitudeNetwork));
+             //   latitudeValueNetwork.setText(String.format("%s", latitudeNetwork));
+                Toast.makeText(getActivity(), R.string.network_provider_update, Toast.LENGTH_SHORT).show();
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+
         public ImageView picture;
         public TextView name;
         public Button btn;
@@ -89,7 +172,7 @@ public class TileContentFragment extends Fragment {
     }
 
     public void populateMatches(ArrayList<GetData> matches) {
-        this.matcheslist = matches;
+    //    this.matcheslist = matches;
     }
 
     /**
@@ -97,7 +180,7 @@ public class TileContentFragment extends Fragment {
      */
     public class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
 
-        
+
         // Set numbers of List in RecyclerView.
         private static final int LENGTH = 6;
 
@@ -111,13 +194,13 @@ public class TileContentFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            int size = matcheslist.size();
-            if(size > 0) {
-                String url = matcheslist.get(position % size).getImageUrl();
-                Picasso.get().load(url).into(holder.picture);
-                holder.name.setText(matcheslist.get(position % size).getName());
-                holder.name.setTag(matcheslist.get(position % size).getUid());
-            }
+          //  int size = matcheslist.size();
+         //   if(size > 0) {
+           //     String url = matcheslist.get(position % size).getImageUrl();
+            //    Picasso.get().load(url).into(holder.picture);
+             //   holder.name.setText(matcheslist.get(position % size).getName());
+            //  holder.name.setTag(matcheslist.get(position % size).getUid());
+          //  }
         }
 
 
